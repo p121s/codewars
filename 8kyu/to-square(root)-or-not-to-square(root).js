c9fd3c0a8e5022a725e47e8c13c1d2a757a33795
p121s <p121s@mail.ru>
2023-10-25 22:29:37 +0300

    To square(root) or not to square(root) function

---
+++ 8kyu/to-square(root)-or-not-to-square(root).js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+// SOLUTION:
+
+const squareOrSquareRoot = array => array.map(a => Math.sqrt(a) % 1 == 0 ? Math.sqrt(a) : (a * a));
+
+console.log(squareOrSquareRoot( [4,3,9,7,2,1]));
