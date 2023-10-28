fa7fc3300db1ef9eb852fdd1a26d09ee22d0418f
p121s <p121s@mail.ru>
2023-10-28 13:04:27 +0300

    Fake Binary

---
+++ 8kyu/fake-binary.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+// SOLUTION:
+
+function fakeBin(x){
+    return x.split('').map(item => item < 5 ? 0 : 1).join('');
+  }
+
+  console.log(fakeBin('023682598'));
